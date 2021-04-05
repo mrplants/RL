e11206dbc70b12f2ff3d5c3ec0ead7349fe236d7
Sean Fitzgerald <fitzgerald.sean.t@gmail.com>
2021-04-06 08:40:40 +0900

    fixed bug in environment init file.

--- environments/__init__.py
+++ environments/__init__.py
@@ -1,2 +1,2 @@
-from frozen_lake import FrozenLake4x4, FrozenLake8x8
-from mountain_car import MountainCar
+from RL.environments.frozen_lake import FrozenLake4x4, FrozenLake8x8
+from RL.environments.mountain_car import MountainCar
